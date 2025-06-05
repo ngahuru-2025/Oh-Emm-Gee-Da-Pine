bc4db21ea290fa74e41047fc94241666b63572c1
Oranee-Kasem-Whai-25 <ornks43@gmail.com>
2025-06-05 20:30:58 +1200

    app' '

--- client/routes.tsx
+++ client/routes.tsx
@@ -2,6 +2,7 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App'
 import LandingPage from './components/LandingPage'
+import Review from './components/Review'
 
 const routes = createRoutesFromElements(
   <>
@@ -9,8 +10,8 @@ const routes = createRoutesFromElements(
     <Route index element={<LandingPage />} />{' '}
     {/* Route for the products page */}
     <Route path="/products" element={<App />} />{' '}
-    {/* Route for the reviews page */}
-    <Route></Route>
+    {/* Route for the reviews page - assuming review needs product id */}
+    <Route path="/reviews/:id" element={<Review />} />
     {/* Route to 404 */}
     <Route path="*" element={<h1>404 - Page Not Found</h1>} />
   </>,
