0ad8aeb3242fd261bab13c8ac57fda2c421a48d4
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-05 19:03:58 +1200

    Created seed for reviews

---
+++ server/db/seeds/reviews.js
@@ -0,0 +1,15 @@
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('reviews').del()
+
+  // Inserts seed entries
+  await knex('reviews').insert([
+    {
+      id: 1,
+      user_id: 3,
+      product_id: 4,
+      title: 'I love my birb!',
+      description: 'Enter description',
+    },
+  ])
+}
