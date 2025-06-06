94fef8e2d4ba290a15d3a9147ffa9df130ff7f93
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-06-06 12:46:09 +1200

    Maybe fixed the posting reviews? Needs some clean up though

--- server/routes/reviews.ts
+++ server/routes/reviews.ts
@@ -26,18 +26,18 @@ router.get('/:id', async (req, res) => {
 })
 router.post('/', async (req, res) => {
   try {
-   const { user_name, rating, comment, product_id } = req.body
-   const newReview = await db.addReview({
-    user_name,
-    rating,
-    comment,
-    product_id})
+    const { title, rating, description, product_id } = req.body
+    const newReview = await db.addReview({
+      title,
+      rating,
+      description,
+      product_id,
+    })
     res.json(newReview)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
   }
 })
-
 
 export default router
